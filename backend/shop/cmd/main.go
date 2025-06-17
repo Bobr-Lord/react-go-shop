1be15176361a63e16488c3b74034cd2ddfcea94f
alexey <molokanov-oleksiy@yandex.ru>
2025-06-17 18:25:58 +0300

    added configs

--- backend/shop/cmd/main.go
+++ backend/shop/cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/Bobr-Lord/react-go-shop/tree/main/backend/shop/internal/config"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
 
+func main() {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	switch cfg.AppEnv {
+	case "local":
+		logrus.SetFormatter(&logrus.TextFormatter{
+			FullTimestamp: true,
+		})
+		logrus.SetLevel(logrus.DebugLevel)
+	case "prod":
+		gin.SetMode(gin.ReleaseMode)
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+	logrus.Infof("config: %+v", cfg)
 }
