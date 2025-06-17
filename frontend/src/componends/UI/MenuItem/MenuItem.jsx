9f1a0808a8d3f1807b789ab5121375d572279daf
alexey <molokanov-oleksiy@yandex.ru>
2025-06-17 23:02:43 +0300

    fix Admin panel

--- frontend/src/componends/UI/MenuItem/MenuItem.jsx
+++ frontend/src/componends/UI/MenuItem/MenuItem.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import ProductCard from "../ProductCard/ProductCard";
+import cl from "./MenuItem.module.css"
 
-const MenuItem = (items) => {
+const MenuItem = ({products, setProducts}) => {
+    const handleDelete = (id) => {
+        setProducts(products.filter(p => p.id !== id));
+    };
     return (
-        <div>
-            
+        <div className={cl.grid}>
+            {products.map(product => (
+                <div key={product.id} className={cl.cardWrapper}>
+                    <ProductCard
+                        title={product.title}
+                        price={product.price}
+                        description={product.description}
+                        image={product.image}
+                        onDelete={() => handleDelete(product.id)}
+                    />
+                </div>
+            ))}
         </div>
     );
 };
--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import cl from './Admin.module.css';
-import ProductCard from "../../componends/UI/ProductCard/ProductCard";
-import MyInput from "../../componends/UI/MyInput/MyInput";
-import MyButton from "../../componends/UI/MyButton/MyButton";
 import AddItemForm from "../../componends/UI/AddItemForm/AddItemForm";
+import MenuItem from "../../componends/UI/MenuItem/MenuItem";
 
 const Admin = () => {
     const [products, setProducts] = useState([]);
@@ -15,29 +13,12 @@ const Admin = () => {
     });
 
 
-    const handleDelete = (id) => {
-        setProducts(products.filter(p => p.id !== id));
-    };
+
     return (
         <div className={cl.admin}>
             <h2>Панель администратора</h2>
             <AddItemForm form={form} setForm={setForm} products={products} setProducts={setProducts} />
-
-            <div className={cl.grid}>
-                {products.map(product => (
-                    <div key={product.id} className={cl.cardWrapper}>
-                        <ProductCard
-                            title={product.title}
-                            price={product.price}
-                            description={product.description}
-                            image={product.image}
-                        />
-                        <button className={cl.deleteButton} onClick={() => handleDelete(product.id)}>
-                            &times;
-                        </button>
-                    </div>
-                ))}
-            </div>
+            <MenuItem products={products} setProducts={setProducts} />
         </div>
     );
 };
